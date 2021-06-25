fdff1b40913a5939fd8b3a1ba1fc85c08835f970
Lupan Vasile <66137225+Vascan5123@users.noreply.github.com>
2021-06-25 21:43:20 +0300

    2

--- js/src/forum/components/IndexPage.js
+++ js/src/forum/components/IndexPage.js
@@ -29,6 +29,8 @@ export default function () {
 
         for (let i = 0; i < app.store.all('tags').length; i++) {
             let id = -1;
+            const sortMap = app.discussions.sortMap();
+
             switch (app.store.all('tags')[i].data.attributes.name) {
                 case "Noutăți":
                     id = app.store.all('tags')[i].data.id;
@@ -76,7 +78,6 @@ export default function () {
                     break;
                 case "Canale":
 
-                    const sortMap = app.discussions.sortMap();
 
                     let sortOptionsCanale = [];
 
@@ -135,64 +136,10 @@ export default function () {
                             )
                         );
                     }
-
-
-
-                    /* id = app.store.all('tags')[i].data.id;
-                    sidebarItems.add(
-                        app.store.all('tags')[i].data.attributes.name,
-                        LinkButton.component(
-                            {
-                                icon: app.store.all('tags')[i].data.attributes.icon,
-                                href: '/press/t/' + app.store.all('tags')[i].data.attributes.slug,
-                                className: 'Button Button--primary tags_left',
-                                itemClassName: 'App-primaryControl',
-                            },
-                            app.store.all('tags')[i].data.attributes.name
-                        ),
-                    );
-
-                    if (app.store.all('tags')[i].data.relationships) {
-                        if (app.store.all('tags')[i].data.relationships.children) {
-
-                            for (let x = 0; x < app.store.all('tags')[i].data.relationships.children.data.length; x++) {
-
-                                let id_tag_children = app.store.all('tags')[i].data.relationships.children.data[x].id;
-
-                                for (let z = 0; z < app.store.all('tags').length; z++) {
-                                    if (app.store.all('tags')[z].data.id == id_tag_children) {
-                                        sidebarItems.add(
-                                            app.store.all('tags')[z].data.attributes.name,
-                                            LinkButton.component(
-                                                {
-                                                    icon: "",
-                                                    href: '/press/t/' + app.store.all('tags')[z].data.attributes.slug,
-                                                    className: 'Button Button--primary tags_left_child',
-                                                    itemClassName: 'App-primaryControl',
-                                                },
-                                                app.store.all('tags')[z].data.attributes.name
-                                            ),
-                                        );
-                                    }
-                                }
-                            }
-                        }
-                    } */
                     break;
                 case "Emisiuni":
-                    id = app.store.all('tags')[i].data.id;
-                    sidebarItems.add(
-                        app.store.all('tags')[i].data.attributes.name,
-                        LinkButton.component(
-                            {
-                                icon: app.store.all('tags')[i].data.attributes.icon,
-                                href: '/t/' + app.store.all('tags')[i].data.attributes.slug,
-                                className: 'Button Button--primary tags_left',
-                                itemClassName: 'App-primaryControl',
-                            },
-                            app.store.all('tags')[i].data.attributes.name
-                        ),
-                    );
+
+                    let sortOptionsEmisiuni = [];
 
                     if (app.store.all('tags')[i].data.relationships) {
                         if (app.store.all('tags')[i].data.relationships.children) {
@@ -200,26 +147,54 @@ export default function () {
                             for (let x = 0; x < app.store.all('tags')[i].data.relationships.children.data.length; x++) {
 
                                 let id_tag_children = app.store.all('tags')[i].data.relationships.children.data[x].id;
-                                /* console.log(id_tag_children); */
+
+                                let title_tag_children;
+                                let href_tag_children;
 
                                 for (let z = 0; z < app.store.all('tags').length; z++) {
                                     if (app.store.all('tags')[z].data.id == id_tag_children) {
-                                        sidebarItems.add(
-                                            app.store.all('tags')[z].data.attributes.name,
-                                            LinkButton.component(
-                                                {
-                                                    icon: app.store.all('tags')[z].data.attributes.icon,
-                                                    href: '/t/' + app.store.all('tags')[z].data.attributes.slug,
-                                                    className: 'Button Button--primary tags_left_child',
-                                                    itemClassName: 'App-primaryControl',
-                                                },
-                                                app.store.all('tags')[z].data.attributes.name
-                                            ),
-                                        );
+                                        title_tag_children = app.store.all('tags')[z].data.attributes.name;
+                                        href_tag_children = app.store.all('tags')[z].data.attributes.slug;
                                     }
                                 }
+                                let params = {
+                                    name: title_tag_children,
+                                    href: href_tag_children
+                                }
+                                sortOptionsEmisiuni.push(params);
+
                             }
                         }
+                    }
+
+
+
+                    if (!sidebarItems.has(app.store.all('tags')[i].data.attributes.name)) {
+                        id = app.store.all('tags')[i].data.id;
+                        sidebarItems.add(
+                            app.store.all('tags')[i].data.attributes.name,
+                            Dropdown.component(
+                                {
+                                    icon: app.store.all('tags')[i].data.attributes.icon,
+                                    buttonClassName: 'Button Button--primary tags_left',
+                                    label: "Emisiuni",
+                                    accessibleToggleLabel: "Emisiuni",
+                                },
+                                Object.keys(sortOptionsEmisiuni).map((value) => {
+                                    const label = sortOptionsEmisiuni[value];
+                                    const active = (app.search.params().sort || Object.keys(sortMap)[0]) === value;
+
+                                    return LinkButton.component(
+                                        {
+                                            icon: active ? 'fas fa-check' : true,
+                                            href: '/t/' + label.href,
+                                            active: active,
+                                        },
+                                        label.name
+                                    );
+                                })
+                            )
+                        );
                     }
                     break;
                 default:
@@ -284,7 +259,7 @@ export default function () {
 
         const sortMap = app.discussions.sortMap();
 
-        const sortOptionsAn = [
+        /* const sortOptionsAn = [
             "2021",
             "2020",
             "2019",
@@ -297,33 +272,71 @@ export default function () {
             "2012",
             "2011",
             "2010",
-        ];
+        ]; */
 
-        if (!items.has('An')) {
-            items.add(
-                'An',
-                Dropdown.component(
-                    {
-                        buttonClassName: 'Button Button--primary tags_center',
-                        label: "An",
-                        accessibleToggleLabel: "An",
-                    },
-                    Object.keys(sortOptionsAn).map((value) => {
-                        const label = sortOptionsAn[value];
-                        const active = (app.search.params().sort || Object.keys(sortMap)[0]) === value;
+        let sortOptionsAn = [];
 
-                        return LinkButton.component(
+        for (let i = 0; i < app.store.all('tags').length; i++) {
+            let id = -1;
+            const sortMap = app.discussions.sortMap();
+
+            switch (app.store.all('tags')[i].data.attributes.name) {
+                case "An":
+                    if (app.store.all('tags')[i].data.relationships) {
+                        if (app.store.all('tags')[i].data.relationships.children) {
+
+                            for (let x = 0; x < app.store.all('tags')[i].data.relationships.children.data.length; x++) {
+
+                                let id_tag_children = app.store.all('tags')[i].data.relationships.children.data[x].id;
+
+                                let title_tag_children;
+                                let href_tag_children;
+
+                                for (let z = 0; z < app.store.all('tags').length; z++) {
+                                    if (app.store.all('tags')[z].data.id == id_tag_children) {
+                                        title_tag_children = app.store.all('tags')[z].data.attributes.name;
+                                        href_tag_children = app.store.all('tags')[z].data.attributes.slug;
+                                    }
+                                }
+                                let params = {
+                                    name: title_tag_children,
+                                    href: href_tag_children
+                                }
+                                sortOptionsAn.push(params);
+
+                            }
+                        }
+                    }
+
+                    items.add(
+                        app.store.all('tags')[i].data.attributes.name,
+                        Dropdown.component(
                             {
-                                icon: active ? 'fas fa-check' : true,
-                                href: '/t/' + label,
-                                active: active,
+                                icon: app.store.all('tags')[i].data.attributes.icon,
+                                buttonClassName: 'Button Button--primary tags_center',
+                                label: "An",
+                                accessibleToggleLabel: "An",
                             },
-                            label
-                        );
-                    })
-                )
-            );
+                            Object.keys(sortOptionsAn).map((value) => {
+                                const label = sortOptionsAn[value];
+                                const active = (app.search.params().sort || Object.keys(sortMap)[0]) === value;
+
+                                return LinkButton.component(
+                                    {
+                                        icon: active ? 'fas fa-check' : true,
+                                        href: '/t/' + label.href,
+                                        active: active,
+                                    },
+                                    label.name
+                                );
+                            })
+                        )
+                    );
+                    break;
+            }
         }
+
+        
 
         const sortOptionsLuna = [
             "Ianuarie",
